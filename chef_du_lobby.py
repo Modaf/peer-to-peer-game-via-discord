9d10443db40d4ce298a3587847a07bfa91faaf1c
Modaf <valentin.pasquale@orange.fr>
2018-07-11 14:55:43 +0200

    Add files via upload

--- chef_du_lobby.py
+++ chef_du_lobby.py
@@ -6,38 +6,51 @@ Pour résoudre cette preuve et être accepté : json : id = solve, cle = le_truc
 
 import discord
 import random
+import hashlib
+import json
 TOKEN = 'NDU4Mjg2Mjk2MTAwNTAzNTUz.DglckA.Kmgp9Jxk5nhotxcDLu9ytZoTR-M'
 IDSALON = 458770508616564741
 
-print("Chef du lobby, celui qui permets d'ajouter des joueurs", int(sys.argv[1]))
+print("Chef du lobby, celui qui permets d'ajouter des joueurs")
 
 global client
 client = discord.Client()
 channel = discord.Object(id=IDSALON)
 
+JOUEURS = 0
 difficulte = 2**(256)/(10**6)
 liste_entrants = []
 reputation = random.random()
 
 def hashit(s) :
-    return hashlib.sha256(s.encode()).hexdigest()
+    return int(hashlib.sha256(s.encode()).hexdigest(), 16)
 
 @client.event
 async def on_message(message):
-    msg = json.loads(message.content)
+    global reputation
+    msg = message.content
     if msg == "//join" :
         _random = random.random()
         nouvelle_reputation = random.random()
-        message = '{"string" : '+str(_random)+', "difficulty" : '+str(difficulte)+', "reputation_avant" : '+str(reputation)+', "reputation" : '+str(nouvelle_reputation) + '}'
+        message = '{"id" : "solve_pow", "string" : '+str(_random)+', "difficulty" : '+str(difficulte)+', "reputation_avant" : '+str(reputation)+', "reputation" : '+str(hasit(str(nouvelle_reputation))) + '}'
         reputation = nouvelle_reputation
-        await client.send_message(channel, message)
         liste_entrants.append(_random)
+    #Sinon on regarde si c'est un objet json
+    msg = json.loads(message.content)
     if msg["id"] == "solve" :
         if msg["cle"] in liste_entrants :
             #On vérifie la preuve de travail
-            if hashit(msg["cle"]+msg["nonce"]) <= difficulte :
+            if hashit(str(msg["cle"])+str(msg["nonce"])) <= difficulte :
                 #C'est ok
                 print("Nouvel arrivant")
+                #On supprime la cle
+                del(liste_entrants[liste_entrants.index(msg["cle"])])
+                #On ajoute un joueur
+                JOUEURS += 1
+                _random = random.random()
+                nouvelle_reputation = random.random()
+                message = '{"id" : "ajout_joueur", "joueurs" : '+str(JOUEURS)+', "reputation_avant" : '+str(reputation)+', "reputation" : '+str(hasit(str(nouvelle_reputation))) + '}'
+                reputation = nouvelle_reputation
 
 @client.event
 async def on_ready():
@@ -45,11 +58,20 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.send_message(channel, '{"id" : "debut", "reputation" : ' + str(hashit(str(reputation))) + '}')
 
 
 client.run(TOKEN)
-
-
+##
+def solve(s, difficulte) :
+    import random
+    s = str(s)
+    while True :
+        _r = random.random()
+        if hashit(s + str(_r)) < difficulte :
+            print("Preuve de travail trouvée :", _r)
+            #On renvoit le json qui permets de nous intégrer au jeu
+            return '{"id" : "solve", "cle" : '+ s + ', "nonce" : '+ str(_r) + '}'
 
 
 
--- class_discord.py
+++ class_discord.py
@@ -23,7 +23,9 @@ except :
     nb_joueurs = 2
     
 
-
+a_join = False
+recherche = False #Regarde si on est en train de chercher une preuve de travail ou non
+reputaion = None #La reputation du chef du lobby
 global client
 client = discord.Client()
 channel = discord.Object(id=IDSALON)
@@ -39,36 +41,55 @@ def random_dest(indice, nb_joueurs) :
 
 @client.event
 async def on_message(message):
-    msg = json.loads(message.content)
-    #On regarde si c'est l'horloge
-    if msg["id"] == "clock" :
-        #Mise à jour de l'horloge interne
-        me.temps = int(msg["counter"])
-        #On regarde l'action qu'on doit effectuer
-        #Si on doit partager notre graphe
-        if msg["action"] == "gossip" and msg["counter"]%nb_joueurs == me.id :  #TODO : random.random() < PROPORTION_GOSSIP :
-            print("Partage de notre dag")
-            message = '{"id" : "dag", "expediteur" : '+str(me.id)+',"destinataire" : '+str(random_dest(me.id, nb_joueurs))+', "dag" : '+me.partage()+', "temps" : '+str(me.temps)+'}' #TODO : prendre un nombre au hasard pour jouer avec plus de joueurs
-            await client.send_message(channel, message)
-        #Si on doit afficher notre dag
-        if msg["action"] == "graphe" : #TODO : les graphes marchent pas
-            me.dag.graphique(True, True)
-            me.dag.graphique(True, False)
-            me.dag.graphique(False)
-    #On regade si un autre joueur veut communiquer avec nous
-    if msg["id"] == "dag" and  msg["destinataire"] == me.id :
-        print("Mise à jour de notre dag")
-        print(me.partage())
-        me.sync_dag(msg["dag"])
-        #On ajoute un noeuds à notre graphe, on est destinataire
-        me.dag.ajout(msg["expediteur"], me.id, [], msg["temps"])
-        print("Mise à jour effectuée")
-        print(me.partage())
-        print("")
-        print("------")
-        print("")
-        #Affichae du dag
-        #me.dag.graphique_propre()
+    if not a_join :
+        msg = json.loads(message.content)
+        #Si c'est le début du jeu
+        if msg["id"] == "debut" :
+            global reputation
+            reputation = msg["reputation"]
+            #On demande une preuve de travail
+            await client.send_message(channel, '//join')
+        #Si c'est pour chercher une preuve de travail
+        if msg["id"] == "solve_pow" and recherche == False :
+            s = str(msg["string"])
+            difficulte = msg["difficulte"]
+            while True :
+                _r = random.random()
+                if hashit(s + str(_r)) < difficulte :
+                    print("Preuve de travail trouvée :", _r)
+                    #On renvoit le json qui permets de nous intégrer au jeu
+                    await client.send_message(channel, '{"id" : "solve", "cle" : '+ s + ', "nonce" : '+ str(_r) + '}')
+    if a_join :
+        msg = json.loads(message.content)
+        #On regarde si c'est l'horloge
+        if msg["id"] == "clock" :
+            #Mise à jour de l'horloge interne
+            me.temps = int(msg["counter"])
+            #On regarde l'action qu'on doit effectuer
+            #Si on doit partager notre graphe
+            if msg["action"] == "gossip" and msg["counter"]%nb_joueurs == me.id :  #TODO : random.random() < PROPORTION_GOSSIP :
+                print("Partage de notre dag")
+                message = '{"id" : "dag", "expediteur" : '+str(me.id)+',"destinataire" : '+str(random_dest(me.id, nb_joueurs))+', "dag" : '+me.partage()+', "temps" : '+str(me.temps)+'}' #TODO : prendre un nombre au hasard pour jouer avec plus de joueurs
+                await client.send_message(channel, message)
+            #Si on doit afficher notre dag
+            if msg["action"] == "graphe" : #TODO : les graphes marchent pas
+                me.dag.graphique(True, True)
+                me.dag.graphique(True, False)
+                me.dag.graphique(False)
+        #On regade si un autre joueur veut communiquer avec nous
+        if msg["id"] == "dag" and  msg["destinataire"] == me.id :
+            print("Mise à jour de notre dag")
+            print(me.partage())
+            me.sync_dag(msg["dag"])
+            #On ajoute un noeuds à notre graphe, on est destinataire
+            me.dag.ajout(msg["expediteur"], me.id, [], msg["temps"])
+            print("Mise à jour effectuée")
+            print(me.partage())
+            print("")
+            print("------")
+            print("")
+            #Affichae du dag
+            #me.dag.graphique_propre()
 @client.event
 async def on_ready():
     print('Logged in as')
