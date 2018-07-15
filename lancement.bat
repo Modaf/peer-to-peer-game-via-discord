4ef73b96af2297c3edb57ad0121941f093c445aa
Modaf <valentin.pasquale@orange.fr>
2018-07-15 21:42:28 +0200

    Delete lancement.bat

--- lancement.bat
+++
@@ -1,5 +0,0 @@
-start py "C:/Users/apzoeiruty/Desktop/peer_to_peer_game/class_discord.py" 0 3
-start py "C:/Users/apzoeiruty/Desktop/peer_to_peer_game/class_discord.py" 1 3
-start py "C:/Users/apzoeiruty/Desktop/peer_to_peer_game/class_discord.py" 2 3
-start py "C:/Users/apzoeiruty/Desktop/peer_to_peer_game/chef_du_lobby.py" 3
-start py "C:/Users/apzoeiruty/Desktop/peer_to_peer_game/logical_clock.py" 5
